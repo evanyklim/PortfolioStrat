735b9889beb08f9e6ddd40edd1aeb2444baa8781
Evan Lim <evanyklim@gmail.com>
2015-11-13 17:26:39 -0500

    gulpfile build added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 
+gulp.task('build', function() {
+  console.log('supposedly building...');
+});
+
 // Testing server side code
 gulp.task('test', function() {
   return gulp.src('./tests/*.spec.js', {
